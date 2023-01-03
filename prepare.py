4da0adceb98c7ed16bf37b37668d37a74bed4dff
Manuel Parra <manuel.parra1@gmail.com>
2023-01-03 16:24:13 -0600

    model phase of project

--- prepare.py
+++ prepare.py
@@ -65,22 +65,3 @@ def prep_telco(df):
     df = pd.concat([df, categorical_columns_binary_dummies], axis=1)
     df = pd.concat([df, categorical_columns_not_binary_dummies], axis=1)
     return df
-
-def split_dataset(df):
-    import pandas as pd
-    import numpy as np
-
-    from sklearn.model_selection import train_test_split
-    '''
-    returns train, validate, test (in that order)
-    
-    uses train_test_split() from SciKit Learn - Model Selection
-    
-    20% test, 80% train_validate
-    then of the 80% train_validate: 30% validate, 70% train.
-    '''
-
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
-
-    return train,validate,test
